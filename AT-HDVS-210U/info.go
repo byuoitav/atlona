3eba11682993a51ca8d67dde0e9acbe62ac396ca
Andrew Werner <klinc9@gmail.com>
2020-10-26 13:38:49 -0600

    renaming directory

---
+++ AT-HDVS-210U/info.go
@@ -0,0 +1,12 @@
+package athdvs210u
+
+import (
+	"context"
+	"fmt"
+)
+
+//Info .
+func (vs *AtlonaVideoSwitcher2x1) Info(ctx context.Context) (interface{}, error) {
+	var info interface{}
+	return info, fmt.Errorf("not currently implemented")
+}
