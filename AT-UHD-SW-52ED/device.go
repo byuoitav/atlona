af61f08f68a48d7216d59535b690ee4e81a984ea
SMR <orcrist@OITAVComputerProgrammer.byu.edu>
2021-07-19 09:11:08 -0600

    Vol Mute and IO

--- AT-UHD-SW-52ED/device.go
+++ AT-UHD-SW-52ED/device.go
@@ -2,62 +2,44 @@ package atuhdsw52ed
 
 import (
 	"context"
-	"fmt"
-	"sync"
+	"net"
 	"time"
 
-	"github.com/byuoitav/wspool"
-	"github.com/gorilla/websocket"
+	"github.com/byuoitav/connpool"
+	"go.uber.org/zap"
+)
+
+const (
+	asciiCarriageReturn = 0x0d
+	asciiLineFeed       = 0x0a
 )
 
 type AtlonaVideoSwitcher5x1 struct {
-	Username string
-	Password string
-	Address  string
-	once     sync.Once
-	pool     wspool.Pool
-	Logger   wspool.Logger
+	pool *connpool.Pool
+	log  *zap.Logger
 }
 
-type room struct {
-	Jsonrpc string `json:"jsonrpc"`
-	ID      string `json:"id"`
-	Result  struct {
-		AVSettings struct {
-			Source          string `json:"source"`
-			Autoswitch      int    `json:"Autoswitch"`
-			Volume          string `json:"Volume"`
-			HDMIAudioMute   int    `json:"HDMI Audio Mute"`
-			HDBTAudioMute   int    `json:"HDBT Audio Mute"`
-			AnalogAudioMute int    `json:"Analog Audio Mute"`
-		} `json:"AV Settings"`
-	} `json:"result"`
-}
-
-func (vs *AtlonaVideoSwitcher5x1) createPool() {
-	if vs.Logger != nil {
-		vs.Logger.Infof("creating pool")
+func NewAtlonaVideoSwitcher5x1(addr string, opts ...Option) *AtlonaVideoSwitcher5x1 {
+	options := &options{
+		ttl:   30 * time.Second,
+		delay: 500 * time.Millisecond,
+		log:   zap.NewNop(),
 	}
 
-	vs.pool = wspool.Pool{
-		NewConnection: createConnectionFunc(vs.Address),
-		TTL:           10 * time.Second,
-		Delay:         75 * time.Millisecond,
-		Logger:        vs.Logger,
+	for _, o := range opts {
+		o.apply(options)
 	}
 
-}
-
-func createConnectionFunc(address string) wspool.NewConnectionFunc {
-	return func(ctx context.Context) (*websocket.Conn, error) {
-		dialer := &websocket.Dialer{}
-		ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
-		defer cancel()
-
-		ws, _, err := dialer.DialContext(ctx, fmt.Sprintf("ws://%s:543", address), nil)
-		if err != nil {
-			return nil, fmt.Errorf("failed to open websocket: %s", err.Error())
-		}
-		return ws, nil
+	return &AtlonaVideoSwitcher5x1{
+		log: options.log,
+		pool: &connpool.Pool{
+			TTL:   options.ttl,
+			Delay: options.delay,
+			NewConnection: func(ctx context.Context) (net.Conn, error) {
+				dial := net.Dialer{}
+				return dial.DialContext(ctx, "tcp", addr+":23")
+			},
+			Logger: options.log.Sugar(),
+		},
 	}
 }
